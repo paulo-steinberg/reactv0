b253691a881b2f69a03aead529bc67cc4c7d40b4
Paulo Steinberg <pvcsteinberg@hotmail.com>
2018-10-13 17:33:50 -0300

    First Element

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 /* jshint esversion: 6 */
-import react from "react";
-import reactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 
-const element = 
+const element = <h1> Hello World</h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
